ee4fdc6de59cda196628332b2de0482bbb9ad841
Metuendus <josecarlos.corona@outlook.com>
2023-05-29 12:00:23 -0600

    fix bugs

--- {{cookiecutter.project_slug}}/etl/functions.R
+++ {{cookiecutter.project_slug}}/etl/functions.R
@@ -1,34 +1,11 @@
-clean_text <- function(col) {
-  trimws(tolower(stri_trans_general(col,
-                                    id = "Latin-ASCII")))
-}
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(
+      stringi,
+      tidyverse
+)
 
-set_channels <- function(.data, sourceMediumCol) {
-  .data %>%
-    mutate(sourceMedium = tolower({{sourceMediumCol}})) %>% 
-    separate({{sourceMediumCol}}, into = c("source",
-                                           "medium"),
-             sep = " / ",
-             remove = FALSE) %>% 
-    mutate(channel = case_when(
-      grepl("direct.*(no.set|none)",
-            {{sourceMediumCol}}) ~ "Direct",
-      grepl("^organic$",
-            medium) ~ "Organic Search",
-      grepl("^(social|social-network|social-media|sm|social network|social media)$",
-            medium) ~ "Social",
-      grepl("email",
-            medium) ~ "Email",
-      grepl("affiliate",
-            medium) ~ "Affiliates",
-      grepl("referral",
-            medium) ~ "Referral",
-      grepl("^(cpc|ppc|paidsearch|facebook)$",
-            medium) ~ "Paid Search",
-      grepl("^(cpv|cpa|cpp|content-text)$",
-            medium) ~ "Other Advertising",
-      grepl("^(display|cpm|banner)$",
-            medium) ~ "Display",
-      TRUE ~ "(other)"
-    ))
+clean_text <- function(col) {
+      trimws(tolower(stri_trans_general(col,
+            id = "Latin-ASCII"
+      )))
 }
